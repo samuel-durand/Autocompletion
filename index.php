d45d4d7cb91f8f90fa792c9a6cc593bbf4797937
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-03 17:07:33 +0200

    update all word

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Page d'accueil</title>
+	<link rel="stylesheet" href="style.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script>
 	$(document).ready(function() {
@@ -37,10 +34,13 @@
 	</script>
 </head>
 <body>
-    
-
+	<div class="search-container">
+		<h1>Recherche</h1>
+		<form action="recherche.php" method="GET">
+			<input type="text" id="search" name="search" placeholder="Rechercher...">
+			<button type="submit">Rechercher</button>
+			<div id="search_results"></div>
+		</form>
+	</div>
 </body>
 </html>
-
-
-
--- recherche.php
+++ recherche.php
@@ -1,9 +1,7 @@
-<?php include('config.php'); 
-include('recherche.php');
-
-?>
-
 <?php
+
+include('config.php');
+
 if(isset($_GET['search'])) {
 	$search = $_GET['search'];
 	$query = $bdd->prepare('SELECT * FROM animaux WHERE nom LIKE :search OR espece LIKE :search ORDER BY nom');
@@ -11,44 +9,24 @@ if(isset($_GET['search'])) {
 	$resultats = $query->fetchAll();
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Résultats de recherche</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<style>
-		.container {
-			margin-top: 50px;
-		}
-	</style>
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 offset-md-3">
-				<form method="GET">
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" id="search" name="search" placeholder="Recherche..." value="<?php echo $search; ?>">
-						<div class="input-group-append">
-							<button class="btn btn-primary" type="submit">Rechercher</button>
-						</div>
-					</div>
-					<div id="search_results"></div>
-				</form>
-				<?php if(isset($search)): ?>
-					<h2>Résultats :</h2>
-					<ul class="list-group">
-						<?php foreach($resultats as $resultat): ?>
-							<li class="list-group-item"><a href="element.php?id=<?php echo $resultat['id']; ?>"><?php echo $resultat['nom']; ?></a></li>
-						<?php endforeach; ?>
-					</ul>
-				<?php endif; ?>
-			</div>
-		</div>
-	</div>
+	<form  method="GET">
+		<input type="text" id="search" name="search" placeholder="Recherche..." value="<?php echo $search; ?>">
+		<button type="submit">Rechercher</button>
+		<div id="search_results"></div>
+	</form>
+	<?php if(isset($search)): ?>
+		<h2>Résultats :</h2>
+		<ul>
+			<?php foreach($resultats as $resultat): ?>
+				<li><a href="element.php?id=<?php echo $resultat['id']; ?>"><?php echo $resultat['nom']; ?></a></li>
+			<?php endforeach; ?>
+		</ul>
+	<?php endif; ?>
 </body>
 </html>
-
-
